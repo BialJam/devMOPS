3eebe6e3a867f7dd738adfc662462641442032ec
Bartosz Kraszewski <bkraszewski@gmail.com>
2016-05-20 19:06:47 +0200

    cleanup

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -5,40 +5,46 @@ import MoveableObject from 'objects/MoveableObject';
 
 class GameState extends Phaser.State {
 
-	preload(){
-		this.game.load.image('background', 'assets/background.png');
-		this.game.load.image('item', 'assets/item.png');
-    this.game.load.image('moveableObject', 'assets/spritesheet.png');
-	}
+    preload() {
+        this.game.load.image('background', 'assets/background.png');
+        this.game.load.image('item', 'assets/item.png');
+        this.game.load.image('moveableObject', 'assets/spritesheet.png');
+    }
 
-  create() {
-    let center = { x: this.game.world.centerX, y: this.game.world.centerY };
-    this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-    this._text.anchor.set(0.5);
+    create() {
+        let center = {x: this.game.world.centerX, y: this.game.world.centerY};
+        this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+        this._text.anchor.set(0.5);
 
-    let object = new MoveableObject(this.game, center.x, center.y);
-    this.game.add.existing(object);
-    this.game.physics.enable(object);
+        let object = new MoveableObject(this.game, center.x, center.y);
+        this.game.add.existing(object);
+        this.game.physics.enable(object);
 
-	  this.item = this.game.add.sprite(0,0,'item');
-	  this.item.inputEnabled = true;
-	  this.item.input.enableDrag();
-	  this.item.events.onDragStart.add(this.onDragStart, this);
-	  this.item.events.onDragStop.add(this.onDragStop, this);
-  }
+        this.createToolbox();
+    }
 
-  update() {
-    this._text.x++;
-    this._text.y--;
-  }
+    createToolbox() {
+        const toolboxItem = this.game.add.sprite(0, 0, 'item');
+        toolboxItem.inputEnabled = true;
+        toolboxItem.input.enableDrag();
+        toolboxItem.events.onDragStart.add(this.onDragStart, this);
+        toolboxItem.events.onDragStop.add(this.onDragStop, this);
 
-	onDragStart(item, pointer){
-		console.log(pointer.x);
-	}
+        this.item = toolboxItem;
+    }
 
-	onDragStop(item, pointer){
-		console.log(pointer.x);
-	}
+    update() {
+        this._text.x++;
+        this._text.y--;
+    }
+
+    onDragStart(item, pointer) {
+        console.log(pointer.x);
+    }
+
+    onDragStop(item, pointer) {
+        console.log(pointer.x);
+    }
 }
 
 export default GameState;
