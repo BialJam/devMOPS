8e0c9548e93135508a614f65aef257149ab9176c
Dawid Pura <puradawid@gmail.com>
2016-05-20 19:47:58 +0200

    Removed RainbowText class totally

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,6 +1,5 @@
 /* jshint esversion: 6 */
 
-import RainbowText from 'objects/RainbowText';
 import MoveableObject from 'objects/MoveableObject';
 import DraggableItem from 'objects/DraggableItem';
 
@@ -14,8 +13,6 @@ class GameState extends Phaser.State {
 
   create() {
     let center = { x: this.game.world.centerX, y: this.game.world.centerY };
-    this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-    this._text.anchor.set(0.5);
 
     let object = new MoveableObject(this.game, center.x, center.y);
     this.game.add.existing(object);
@@ -37,8 +34,6 @@ class GameState extends Phaser.State {
     }
 
     update() {
-        this._text.x++;
-        this._text.y--;
     }
 
 	onDragStart(item, pointer){
