2c68f97fe7424277dc18bfb6e48766a555309ae1
Dawid Pura <puradawid@gmail.com>
2016-05-20 17:46:04 +0200

    Added update method (main game loop)

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -5,11 +5,15 @@ import RainbowText from 'objects/RainbowText';
 class GameState extends Phaser.State {
 
 	create() {
-		let center = { x: this.game.world.centerX, y: this.game.world.centerY };
-		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		text.anchor.set(0.5);
+		center = { x: this.game.world.centerX, y: this.game.world.centerY };
+		this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+		this._text.anchor.set(0.5);
 	}
 
+  update() {
+    this._text.x++;
+    this._text.y--;
+  }
 }
 
 export default GameState;
