636c8a2d1a10eea6654c9205550df4a4b7af8cf8
Mateusz <rrandzor@gmail.com>
2016-05-20 19:40:36 +0200

    FYX

--- src/objects/MoveableObject.js
+++ src/objects/MoveableObject.js
@@ -1,18 +1,21 @@
 /* jshint esversion: 6 */
-import {Sprite, Point} from 'Phaser';
+const { Sprite, Point } = Phaser;
 
 class MoveableObject extends Sprite {
 
   constructor(game, x, y) {
     super(game, x, y, 'moveableObject');
     game.physics.enable(this);
+
     const body = this.body;
     body.mass = 100;
     body.velocity = new Point(10, 1);
     body.collideWorldBounds = true;
     body.worldBounce = new Point(1, 1);
-    this.animations.add('walk');
-    this.animations.play('walk', 10, true);
+
+    const animations = this.animations;
+    animations.add('walk');
+    animations.play('walk', 10, true);
   }
 
 }
