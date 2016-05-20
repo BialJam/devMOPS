2d3c505d4dc4d2991d9491b2ae1d6f28dbae59f2
Dawid Pura <puradawid@gmail.com>
2016-05-20 17:56:19 +0200

    Fix issue with let

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -5,7 +5,7 @@ import RainbowText from 'objects/RainbowText';
 class GameState extends Phaser.State {
 
 	create() {
-		center = { x: this.game.world.centerX, y: this.game.world.centerY };
+		let center = { x: this.game.world.centerX, y: this.game.world.centerY };
 		this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
 		this._text.anchor.set(0.5);
 	}
