549157f9341f063353375de80b278bf10070d1e9
Bartosz Kraszewski <bkraszewski@gmail.com>
2016-05-20 19:49:47 +0200

    cleanup again

--- src/objects/DraggableItem.js
+++ src/objects/DraggableItem.js
@@ -3,14 +3,14 @@
  */
 
 
-class DraggableItem extends Phaser.Sprite{
-    constructor(game, x, y){
+class DraggableItem extends Phaser.Sprite {
+    constructor(game, x, y) {
         super(game, x, y, 'item');
 
         this.inputEnabled = true;
         this.input.enableDrag();
         this.game.physics.enable(this);
-        this.body.worldBounde = new Phaser.Point(1,1);
+        this.body.worldBounde = new Phaser.Point(1, 1);
     }
 
 }
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -6,29 +6,23 @@ import DraggableItem from 'objects/DraggableItem';
 
 class GameState extends Phaser.State {
 
-	preload(){
-		this.game.load.image('background', 'assets/background.png');
-		this.game.load.image('item', 'assets/item.png');
-    this.game.load.spritesheet('moveableObject', 'assets/spritesheet.png', 40, 40, 10);
-	}
+    preload() {
+        this.game.load.image('background', 'assets/background.png');
+        this.game.load.image('item', 'assets/item.png');
+        this.game.load.spritesheet('moveableObject', 'assets/spritesheet.png', 40, 40, 10);
+    }
 
-  create() {
-    let center = { x: this.game.world.centerX, y: this.game.world.centerY };
-    this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-    this._text.anchor.set(0.5);
+    create() {
+        let center = {x: this.game.world.centerX, y: this.game.world.centerY};
+        this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+        this._text.anchor.set(0.5);
 
-    let object = new MoveableObject(this.game, center.x, center.y);
-    this.game.add.existing(object);
-    this.game.physics.enable(object);
+        this.createToolbox();
 
-	  this.item = this.game.add.sprite(0,0,'item');
-	  this.item.inputEnabled = true;
-	  this.item.input.enableDrag();
-	  this.item.events.onDragStart.add(this.onDragStart, this);
-	  this.item.events.onDragStop.add(this.onDragStop, this);
-
-    this.createToolbox();
-  }
+        let object = new MoveableObject(this.game, center.x, center.y);
+        this.game.add.existing(object);
+        this.game.physics.enable(object);
+    }
 
     createToolbox() {
         const toolboxItem = new DraggableItem(this.game, 0, 0);
@@ -40,24 +34,6 @@ class GameState extends Phaser.State {
         this._text.x++;
         this._text.y--;
     }
-
-	onDragStart(item, pointer){
-		console.log(pointer.x);
-	}
-
-	onDragStop(item, pointer){
-		console.log(pointer.x);
-	}
-
-  createToolbox() {
-      const toolboxItem = this.game.add.sprite(0, 0, 'item');
-      toolboxItem.inputEnabled = true;
-      toolboxItem.input.enableDrag();
-      toolboxItem.events.onDragStart.add(this.onDragStart, this);
-      toolboxItem.events.onDragStop.add(this.onDragStop, this);
-
-      this.item = toolboxItem;
-  }
 
 }
 
