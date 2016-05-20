9b0ba7047dd6ba50c78bf453e492ab579c740d3e
Dawid Pura <puradawid@gmail.com>
2016-05-20 18:34:25 +0200

    State added

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,14 +1,22 @@
 /* jshint esversion: 6 */
 
 import RainbowText from 'objects/RainbowText';
+import MoveableObject from 'objects/MoveableObject';
 
 class GameState extends Phaser.State {
 
-	create() {
-		let center = { x: this.game.world.centerX, y: this.game.world.centerY };
-		this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		this._text.anchor.set(0.5);
-	}
+  preload() {
+    this.game.load.image('moveableObject', 'assets/spritesheet.png');
+  }
+
+  create() {
+    let center = { x: this.game.world.centerX, y: this.game.world.centerY };
+    this._text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+    this._text.anchor.set(0.5);
+
+    let object = new MoveableObject(this.game, center.x, center.y);
+    this.game.add.existing(object);
+  }
 
   update() {
     this._text.x++;
