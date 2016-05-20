45114a1ccfe733034535903ed1cdeb57308788b6
Bartosz Kraszewski <bkraszewski@gmail.com>
2016-05-20 18:21:56 +0200

    sample sprite

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,13 +1,20 @@
 /* jshint esversion: 6 */
 
-import RainbowText from 'objects/RainbowText';
+//import RainbowText from 'objects/RainbowText';
 
 class GameState extends Phaser.State {
 
+	preload(){
+		this.game.load.image('background', 'assets/background.png');
+	}
+
 	create() {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY };
-		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-		text.anchor.set(0.5);
+		//let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+		//text.anchor.set(0.5);
+
+		this.game.add.sprite(0,0,'background');
+
 	}
 
 }
