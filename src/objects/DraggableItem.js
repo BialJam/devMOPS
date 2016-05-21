e8916bbb4c57aca1518412432efb501ca4cfb840
Mateusz <rrandzor@gmail.com>
2016-05-21 14:57:42 +0200

    Drag merge

--- src/objects/DraggableItem.js
+++ src/objects/DraggableItem.js
@@ -2,14 +2,16 @@
 
 class DraggableItem extends Phaser.Sprite {
     constructor(game, x, y, collisionGroup) {
-        super(game, x, y, 'item_rotated');
-        this._collisionGroup = collisionGroup;
+      super(game, x, y, 'item_rotated');
+      this._collisionGroup = collisionGroup;
+      this.draggable = true;
     }
 
     enablePhysics() {
         this.game.physics.p2.enable(this, true);
 
         const body = this.body;
+        body.mass = 99999;
         body.clearShapes();
         body.loadPolygon('item_rotated', 'item_rotated');
         body.setCollisionGroup(this._collisionGroup);
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -26,7 +26,7 @@ class GameState extends Phaser.State {
 
     const center = getCenter(this.game.world);
     game.physics.startSystem(Phaser.Physics.P2JS);
-    this.createToolbox();
+    this.createToolbox(mainCollisionGroup);
 
     this.item = this.createToolbox(secondCollisionGroup);
     this.moveableObject = new MoveableObject(this.game, center.x, center.y, mainCollisionGroup);
@@ -55,9 +55,12 @@ class GameState extends Phaser.State {
 
     // p2 uses different coordinate system, so convert the pointer position to p2's coordinate system
     var physicsPos = [game.physics.p2.pxmi(pointer.position.x), game.physics.p2.pxmi(pointer.position.y)];
-
-    if (bodies.length) {
+    if (bodies.length && bodies[0].parent.sprite.draggable) {
       var clickedBody = bodies[0];
+      this.clickedBody = clickedBody.parent;
+      this.clickedBody.mass = 1;
+      this.clickedBody.clearCollision();
+      // clickedBody.setZeroDamping();
 
       var localPointInBody = [0, 0];
       // this function takes physicsPos and coverts it to the body's local coordinate system
@@ -74,6 +77,9 @@ class GameState extends Phaser.State {
   }
 
   onUp() {
+    this.clickedBody.mass = 99999;
+    this.clickedBody.setZeroVelocity();
+    this.clickedBody.setZeroRotation();
     this.game.physics.p2.removeConstraint(this.mouseConstraint);
   }
 
