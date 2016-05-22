6c863c712f5c55239f1fd8ae57391cb8b1f84f19
Bartosz Kraszewski <bkraszewski@gmail.com>
2016-05-22 13:47:31 +0200

    game intro

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 /* jshint esversion: 6 */
 import GameState from 'states/GameState';
 import Levels from 'objects/Level'
+import GameIntro from 'states/GameIntro'
 
 class Game extends Phaser.Game {
 
   constructor() {
     super(800, 600, Phaser.AUTO, 'content', null);
+    this.state.add('Intro', GameIntro, false);
     this.state.add('GameState', GameState, false);
-    this.startLevel(0, this.state);
+    //this.startLevel(0, this.state);
+    this.state.start('Intro', true, true, {
+      onStart:()=>{
+        this.startLevel(0, this.state);
+      }
+    });
   }
 
   startLevel(currentLevel = 0, stateManager) {
